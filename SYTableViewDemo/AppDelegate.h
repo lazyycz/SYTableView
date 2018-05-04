7274cca12d3182186999d48d596cc9ca365c68e9
余成桂 <yuchenggui@macbook-pro-4.local>
2018-05-04 13:59:24 +0800

    Initial Commit

---
+++ SYTableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYTableViewDemo
+//
+//  Created by Stone.Yu on 2018/5/4.
+//  Copyright © 2018年 Stone.Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYTableViewDemoTests/SYTableViewDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  SYTableViewDemoTests.m
+//  SYTableViewDemoTests
+//
+//  Created by Stone.Yu on 2018/5/4.
+//  Copyright © 2018年 Stone.Yu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SYTableViewDemoTests : XCTestCase
+
+@end
+
+@implementation SYTableViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
